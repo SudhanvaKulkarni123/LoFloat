ad55bfb77bdd5579685653bc8dd6b94f22f5c735
Sudhanva Kulkarni <sudhanvakulkarni@berkeley.edu>
2026-03-05 19:30:20 -0800

    omdified init

--- LoFloat/__init__.py
+++ LoFloat/__init__.py
@@ -1,4 +1,4 @@
 from .LoFloat import virtual_round, RoundingMode, InfBehavior, NaNBehavior, Signedness, FloatFormatDescriptor  # compiled C++ extension
 from .formats import create_p3109_params, create_half_params, create_single_params
 from .layers import LoF_Quantize, LoF_Linear, LoF_Conv2d, _quantize, mantissa_quantize, exp_mant_quantize
-from .utils import set_mantissa_fields, set_exponent_fields, set_exponentbias_fields, lofloatify, set_all_to_half, set_all_to_3109, print_exp_mant, record_formats
+from .utils import set_mantissa_fields, set_exponent_fields, set_exponentbias_fields, lofloatify, set_all_to_half, set_all_to_3109, print_exp_mant, record_formats 
